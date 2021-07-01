48afd76cabaf3312854724f4100c8723ea830fc1
Steven Kao <st.kao.05@gmail.com>
2021-07-01 15:31:54 +0800

    skip animation

--- src/js/section-knowledge.js
+++ src/js/section-knowledge.js
@@ -1,34 +1,71 @@
 import MicroModal from "micromodal";
+import { on } from "delegated-events";
 
 export function mountSectionKnowledge() {
   document.addEventListener("DOMContentLoaded", function () {
-    let currentModal;
-
-    MicroModal.init({
-      disableScroll: true,
-      disableFocus: true,
-      onShow: (modal) => {
-        // allow one modal at the time
-        if (currentModal) {
-          MicroModal.close(currentModal.id);
-        }
-        currentModal = modal;
-      },
-    });
-
-    //document.addEventListener("keydown", (event) => {
-    //if (!currentModal) {
-    //return;
-    //}
-    //if (event.key === "ArrowRight") {
-    //MicroModal.close(currentModal.id);
-    //MicroModal.show(deltaArticleId(currentModal.id, 1));
-    //}
-    //});
+    ModalManager.init();
   });
 }
 
-function deltaArticleId(id, delta) {
+const modalOption = {
+  disableScroll: true,
+  disableFocus: true,
+  openTrigger: "none",
+  closeTrigger: "none",
+};
+
+const ModalManager = {
+  modalId: null,
+
+  init() {
+    on("click", "[data-micromodal-trigger]", (event) => {
+      const modalId = event.target.getAttribute("data-micromodal-trigger");
+      const skipAnimation = event.target.hasAttribute(
+        "data-micromodal-skip-animation"
+      );
+
+      this.show(modalId, { skipAnimation });
+    });
+
+    on("click", "[data-micromodal-close]", () => {
+      this.close();
+    });
+
+    document.addEventListener("keydown", (event) => {
+      if (!this.modalId) {
+        return;
+      }
+      if (event.key === "ArrowRight") {
+        this.show(delta(this.modalId, 1), { skipAnimation: true });
+      }
+      if (event.key === "ArrowLeft") {
+        this.show(delta(this.modalId, -1), { skipAnimation: true });
+      }
+    });
+  },
+  show(id, { skipAnimation } = {}) {
+    if (skipAnimation) {
+      document.documentElement.classList.add("micromodal-skip-animation");
+    } else {
+      document.documentElement.classList.remove("micromodal-skip-animation");
+    }
+
+    if (this.modalId) {
+      MicroModal.close(this.modalId);
+    }
+
+    MicroModal.show(id, modalOption);
+    this.modalId = id;
+  },
+  close() {
+    if (this.modalId) {
+      MicroModal.close(this.modalId);
+    }
+    this.modalId = null;
+  },
+};
+
+function delta(id, delta) {
   const num = parseInt(id.replace("article-", ""));
   return `article-${num + delta}`;
 }
